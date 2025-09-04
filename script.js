8a32bcc212887e99dccfec9966bbe130fe616141
Rohric <emilmarsal@icloud.com>
2025-09-04 16:57:49 +0200

    feat. show  image in Dialog + title is array

--- script.js
+++ script.js
@@ -10,34 +10,64 @@ let festivalImages = [
   "assets/image/RIP.jpg",
   "assets/image/Southside.jpg",
 ];
+let festivalImage = "";
+let dialogRef = "";
+let dialogImgRef = "";
 
 function init() {
+  // Bilder anzeigen
   getAllFestivalImages();
+
+  // Referenzen holen
+  dialogRef = document.getElementById("dialog1");
+  dialogImgRef = document.getElementById("dialog_image");
 }
 
 // Templates..dienen dazu den inhalt aus dem Array anzuzeigen
 
 function getAllFestivalImages() {
-    let festivalImage = "";
-    for (let i = 0; i < festivalImages.length; i++) {
-      festivalImage += `<div onclick="openDialog()"
-      ><img src="${festivalImages[i]}" alt="">
-      </div>`;
-    }
-    document.getElementById("content").innerHTML = festivalImage;
+  let festivalImage = "";
+  for (let i = 0; i < festivalImages.length; i++) {
+    festivalImage += `
+    <div onclick="openDialog(${i})">
+      <img src="${festivalImages[i]}" alt="">
+    </div>`;
   }
-  
-// Dialog 
+  document.getElementById("content").innerHTML = festivalImage;
+}
 
-  const dialogRef = document.getElementById('dialog1');
+// Dialog
 
-  function openDialog() {
-    dialogRef.showModal();
-    dialogRef.classList.add('opened')
-  }
+// function openDialog(i) {
+//   document.getElementById('dialogTitle').textContent = ($[i])
+
+//   dialogImgRef.src = festivalImages[i]; // Bildquelle übertragen 
+//   dialogRef.showModal();
+//   dialogRef.classList.add("opened");
+// }
+
+// KI GENERIERT amfang 
+function openDialog(i) {
+  const path = festivalImages[i];
+  const filename = path.split("/").pop().split(".")[0]; // "beerPapst" // ki generiert verstehe noch nicht wieso 
+
+
+  document.getElementById('dialogTitle').textContent = filename;
+  dialogImgRef.src = path;
+  dialogRef.showModal();
+  dialogRef.classList.add("opened");
+}
+
+//KI Generiert ende
+
+function closeDialog() {
+  dialogRef.close();
+  dialogRef.classList.remove("opened");
+}
+
+function goForward() {
+
   
-  function closeDialog(){
-      dialogRef.close()
-      dialogRef.classList.remove('opened')
-  }
-  
+}
+
+function goBack() {}
